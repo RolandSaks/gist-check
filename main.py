ef401ffc1c4875e8841a91ecb4bc3ba1539c61ed
Roland Saks <roland.saks@inbank.ee>
2021-07-29 16:29:09 +0300

    GIST-5 | test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
+import os
 import time
 import flask
-
 from typing import List
 from cmreslogging.handlers import CMRESHandler
 from flask import Flask,jsonify
@@ -13,10 +13,12 @@ from gitmonitor import GitGistMonitor
 from repository import save_gists, load_gists
 from pipecrmdeal import create_new_deal
 
-USERNAMES = ["antomer", "RolandSaks"]
+USERNAMES = os.environ("GITHUB_USERNAMES")
+
+# logging.basicConfig(level=logging.INFO)
 handler = CMRESHandler(hosts=[{'host': 'elasticsearch', 'port': 9200}],
                            auth_type=CMRESHandler.AuthType.NO_AUTH,
-                           es_index_name="pipedrive_test")                   
+                           es_index_name="pipedrive_test")
 log = logging.getLogger("RolandTest")
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
@@ -26,13 +28,12 @@ app = Flask(__name__)
 
 def callback_func(user, gists: List[GitGist]) -> None:
     log.info("Monitor reported user [%s] gists: %s", user, [x.id for x in gists])
-
     if gists:
         save_gists(gists)
-        id = [x.id for x in gists]
-        for username in id:
-           create_new_deal(user, username)
-     
+        # id = [x.id for x in gists]
+        # for username in id:
+        #    create_new_deal(user, username)
+
 
 def run() -> None:
 
@@ -43,10 +44,10 @@ def run() -> None:
                                                        callback=callback_func,
                                                        interval=400))
     monitor.start()
+    
 
-run()                      
+run()
 
 @app.route("/health", methods=["GET"])
 def heartbeat():
     resp = flask.make_response(jsonify(status="UP"), 200)
-    return resp
