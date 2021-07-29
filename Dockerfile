ab031f31d693804033686e0d94c5209608eeeb47
Roland Saks <roland.saks@inbank.ee>
2021-07-29 14:51:16 +0300

    GIST-5 | Docker files

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,15 @@ FROM python:3.8.5-slim
 
 COPY . /app/
 
+ENV FLASK_APP main.py
+ENV FLASK_RUN_PORT 8080
+ENV FLASK_RUN_HOST 0.0.0.0
+ENV FLASK_ENV development
+
 RUN pip3 install -r /app/requirements.txt
 RUN  apt-get update \
      && apt-get install -y \
         curl
 
 WORKDIR /app/
-CMD python3 main.py
+CMD flask run --host=0.0.0.0
--- wait_to_start.sh
+++ wait_to_start.sh
@@ -7,17 +7,7 @@ is_ready() {
     eval "$WAIT_COMMAND"
 }
 
-# wait until is ready
-i=0
-while ! is_ready; do
-    i=`expr $i + 1`
-    if [ $i -ge $WAIT_LOOPS ]; then
-        echo "$(date) - still not ready, giving up"
-        exit 1
-    fi
-    echo "$(date) - waiting to be ready"
-    sleep $WAIT_SLEEP
-done
+timeout 300 bash -c "until curl --silent --output /dev/null http://elasticsearch:9200/_cat/health?h=st; do printf '.'; sleep 5; done; printf '\n'"
 
 #start the script
 exec $WAIT_START_CMD
